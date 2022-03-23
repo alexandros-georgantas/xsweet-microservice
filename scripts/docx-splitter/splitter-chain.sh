1699c6fd19bee26de3e01ee5fa716b13d94619ca
sukii <suki.venkat@gmail.com>
2022-03-23 23:13:04 +0530

    adding scripts for docx-splitter

---
+++ scripts/docx-splitter/splitter-chain.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#A docx splitter preprocessor shell script for XSweet pipeline
+
+
+# Directory of script
+DIRECTORY=$(cd `dirname $0` && pwd)
+SAXONDIR=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'saxon' -print -quit)
+TYPESCRIPT=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'editoria_typescript*' -print -quit)
+# Note Saxon is included with this distribution, qv for license.
+saxonHE="java -jar ${SAXONDIR}/saxon-he-10.3.jar"  # SaxonHE (XSLT 3.0 processor)
+
+
+DOCXMLDIR=$1
+TOC="${DOCXMLDIR}/word/toc.xml"
+DOCXSPLITTER="${TYPESCRIPT}/docx-splitter/"
+$saxonHE -xsl:$DOCXSPLITTER/split-for-xsweet.xsl -s:$DOCXMLDIR/word/document.xml -o:$TOC
+SPLITFILES=$(cat $TOC|grep -oP "<file>[^<>]+<"|sed "s/<file>//"|tr -d "<")
+for DOCi in $SPLITFILES; do
+    N=$(echo "${DOCi}"|grep -oP "[0-9]+")
+    # Calling XSweet Pipeline here:    
+    ./splitter_execute_chain.sh ${DOCXMLDIR} ${N}"
+done
---
+++ scripts/docx-splitter/splitter_execute_chain.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# Directory of script
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+# Modified from https://gitlab.coko.foundation/XSweet/XSweet_runner_scripts/raw/master/execute_chain.sh
+
+# For producing HTML5 outputs via XSweet XSLT from sources extracted from .docx (Office Open XML)
+
+# is any short identifier
+TEMP=$1
+N=$2
+
+XSWEET=$(find ${DIRECTORY}/../.. -maxdepth 1  -name "XSweet*" -print -quit)
+# TYPESCRIPT=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'editoria_typescript*' -print -quit)
+# HTMLEVATOR=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'HTMLevator*' -print -quit)
+
+SAXONDIR=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'saxon' -print -quit)
+
+echo $XSWEET
+# echo $TYPESCRIPT
+# echo $HTMLEVATOR
+
+# Note Saxon is included with this distribution, qv for license.
+saxonHE="java -jar ${SAXONDIR}/saxon-he-10.3.jar"  # SaxonHE (XSLT 3.0 processor)
+
+# EXTRACTION
+PIPELINE="${XSWEET}/applications/PIPELINE.xsl"                       # "Extraction" stylesheet
+
+XMLTOHTML5="${XSWEET}/applications/html-polish/html5-serialize.xsl"
+
+
+$saxonHE -threads:10 -xsl:$PIPELINE -s:$TEMP/word/document$N.xml -o:$TEMP/outputs/PIPELINED$N.xhtml
+
+if [ $? -eq 0 ]
+then
+  echo "Made PIPELINED${N}.xhtml"
+else
+  echo "There was an error converting the document." >&2
+  exit 1
+fi
+
+$saxonHE -threads:5 -xsl:$XMLTOHTML5 -s:$TEMP/outputs/PIPELINED$N.xhtml -o:$TEMP/outputs/HTML5_$N.html
+
+if [ $? -eq 0 ]
+then
+  echo "Made HTML5_${N}.html"
+  exit 0
+else
+  echo "There was an error converting the document." >&2
+  exit 1
+fi
