6f3dbac9104cef71a68b3c9d164160c2412d2522
sukii <suki.venkat@gmail.com>
2022-03-28 14:36:34 +0530

    adding an editoria HTML splitter

---
+++ scripts/docx-splitter/editoria_splitter_execute_chain.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Directory of script
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+# Modified from https://gitlab.coko.foundation/XSweet/XSweet_runner_scripts/raw/master/execute_chain.sh
+
+# For producing HTML5 outputs via XSweet XSLT from sources extracted from .docx (Office Open XML)
+
+# is any short identifier
+TEMP=$1
+
+XSWEET=$(find ${DIRECTORY}/../.. -maxdepth 1  -name "XSweet*" -print -quit)
+# TYPESCRIPT=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'editoria_typescript*' -print -quit)
+# HTMLEVATOR=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'HTMLevator*' -print -quit)
+
+SAXONDIR=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'saxon' -print -quit)
+
+echo $XSWEET
+# echo $TYPESCRIPT
+# echo $HTMLEVATOR
+
+# Note Saxon is included with this distribution, qv for license.
+saxonHE="java -jar ${SAXONDIR}/saxon-he-10.3.jar"  # SaxonHE (XSLT 3.0 processor)
+
+# EXTRACTION
+PIPELINE="${XSWEET}/applications/PIPELINE.xsl"                       # "Extraction" stylesheet
+
+XMLTOHTML5="${XSWEET}/applications/html-polish/html5-serialize.xsl"
+
+$saxonHE -threads:10 -xsl:$PIPELINE -s:$TEMP/word/document.xml -o:$TEMP/outputs/PIPELINED.xhtml
+
+if [ $? -eq 0 ]
+then
+  echo "Made PIPELINED.xhtml"
+else
+  echo "There was an error converting the document." >&2
+  exit 1
+fi
+
+$saxonHE -xsl:$DOCXSPLITTER/split-html.xsl -s:$TEMP/outputs/PIPELINED.xhtml -o:$TOC
+SPLITFILES=$(cat $TOC|grep -oP "<div [^<>]+><a href='document[0-9]+.html'>[^<>]+<"|sed "s/<div [^<>]+><a href='document[0-9]+.html'>//"|tr -d "<")
+for DOCi in $SPLITFILES; do
+    N=$(echo "${DOCi}"|grep -oP "[0-9]+")
+    # Calling XHTML to HTML5 converter here:    
+    $saxonHE -threads:5 -xsl:$XMLTOHTML5 -s:$TEMP/outputs/document$N.html -o:$TEMP/outputs/HTML5_$N.html
+    if [ $? -eq 0 ]
+       then
+	   echo "Made HTML5_${N}.html"
+	   exit 0
+       else
+	   echo "There was an error converting the document." >&2
+       exit 1
+    fi
+done
+
--- scripts/docx-splitter/splitter-chain.sh
+++ scripts/docx-splitter/splitter-chain.sh
@@ -5,6 +5,7 @@
 # Directory of script
 DIRECTORY=$(cd `dirname $0` && pwd)
 SAXONDIR=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'saxon' -print -quit)
+XSWEET=$(find ${DIRECTORY}/../.. -maxdepth 1  -name "XSweet*" -print -quit)
 TYPESCRIPT=$(find ${DIRECTORY}/../.. -maxdepth 1  -name 'editoria_typescript*' -print -quit)
 # Note Saxon is included with this distribution, qv for license.
 saxonHE="java -jar ${SAXONDIR}/saxon-he-10.3.jar"  # SaxonHE (XSLT 3.0 processor)
@@ -13,6 +14,7 @@ saxonHE="java -jar ${SAXONDIR}/saxon-he-10.3.jar"  # SaxonHE (XSLT 3.0 processor
 DOCXMLDIR=$1
 TOC="${DOCXMLDIR}/word/toc.xml"
 DOCXSPLITTER="${TYPESCRIPT}/docx-splitter/"
+#DOCXSPLITTER="${XSWEET}/applications/docx-splitter/"
 $saxonHE -xsl:$DOCXSPLITTER/split-for-xsweet.xsl -s:$DOCXMLDIR/word/document.xml -o:$TOC
 SPLITFILES=$(cat $TOC|grep -oP "<file>[^<>]+<"|sed "s/<file>//"|tr -d "<")
 for DOCi in $SPLITFILES; do
