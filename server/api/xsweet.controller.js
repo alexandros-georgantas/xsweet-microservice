cacdc5bf06f90cb107410e0dc6ce6fe75b3c3c55
sukii <suki.venkat@gmail.com>
2022-04-06 10:01:32 +0530

    fix(test): adding sync api

--- server/api/xsweet.controller.js
+++ server/api/xsweet.controller.js
@@ -1,7 +1,7 @@
 const { boss } = require('@coko/server')
 const { authenticate } = require('@coko/service-auth')
 
-const { uploadHandler } = require('./helpers')
+const { uploadHandler, conversionHandler } = require('./helpers')
 
 const docxToHTML = async (req, res) => {
   try {
@@ -38,8 +38,37 @@ const docxToHTML = async (req, res) => {
   }
 }
 
+const docxToHTMLSync = async (req, res) => {
+  try {
+    if (req.fileValidationError) {
+      return res.status(400).json({ msg: req.fileValidationError })
+    }
+    if (!req.file) {
+      return res.status(400).json({ msg: 'docx file is not included' })
+    }
+
+    const {
+      serviceCredentialId,
+      serviceCallbackTokenId,
+      bookComponentId,
+      responseToken,
+      callbackURL,
+    } = req.body
+    const { path: filePath } = req.file
+
+    const htmlContent = await conversionHandler(filePath)
+
+    return res.status(200).json({
+      html: htmlContent,
+    })
+  } catch (e) {
+    return res.status(500).json({ msg: e.toString() })
+  }
+}
+
 const xSweetServiceBackend = app => {
   app.post('/api/docxToHTML', authenticate, uploadHandler, docxToHTML)
+  app.post('/api/docxToHTMLSync', authenticate, uploadHandler, docxToHTMLSync)
 }
 
 module.exports = xSweetServiceBackend
